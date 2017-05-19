f7309488c072c67cef3a2ad0e5e23bce40b39072
Mayur Dhikle <mdhikle@gmail.com>
2017-05-18 23:15:13 -0700

    After Initial Commit, Changes made in MainActivity, Control, ctr files (Not tested )

--- app/src/main/java/com/example/android/botcontrol/activity/Control.java
+++ app/src/main/java/com/example/android/botcontrol/activity/Control.java
@@ -1,8 +1,172 @@
 package com.example.android.botcontrol.activity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+
+import com.example.android.botcontrol.R;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import static com.example.android.botcontrol.R.layout.ctr;
+
 /**
  * Created by MayurDhikle on 5/18/17.
  */
 
-public class Control {
+public class Control extends AppCompatActivity{
+
+    private Socket socket = null;
+    private static final String TAG = "Control";
+    public static String ip = null;
+    public static int port = 0;
+    public static boolean isActivity = false;
+    public static boolean isLive = false;
+    private static PrintWriter printWriter = null;
+    private boolean isConnect = false;
+    private Thread thread = null;
+
+    // These two were used before but of no use in the modified code
+    private RadioButton radioButton;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(ctr);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        isActivity = true;
+        isLive = true;
+        isConnect = true;
+        thread = new Thread(runnable);
+        thread.start();
+        Log.d(TAG, "On Create");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Log.d(TAG, "On Options Menu");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*@SuppressLint("ClickableViewAccessibility")
+    private void setBtnAffairs{
+    }*/
+
+    public static void touchHandle(MotionEvent event, String orderStr, String tips){
+        int action = event.getAction();
+        Log.d(TAG, "action = "+action+"touchHandle_one");
+        switch (action){
+            case MotionEvent.ACTION_DOWN:
+                ctrOrder(orderStr+"\n\r",tips);
+                break;
+            case MotionEvent.ACTION_UP:
+                ctrOrder("\n\r", "ͣ"); // try later with tips
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void ctrOrder(String orderStr, String tips){
+        Log.d(TAG, "ctrOrder" + orderStr);
+        if( (orderStr != null) && (printWriter != null) ){
+
+            printWriter.print(orderStr);
+            printWriter.flush();
+        }
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                Log.d(TAG, "Trim runnable");
+                if (Control.ip != null && Control.port != 0){
+                    Log.i(TAG, "Control.ip =" + Control.ip);
+                    Log.i(TAG, "Control.port =" + Control.port);
+                }
+                else
+                    socket = new Socket("192.168.8.1", 2001);
+
+                OutputStream os = socket.getOutputStream();
+                printWriter = new PrintWriter(os, true);
+            }
+            catch (Exception e) {
+                return;
+            }
+        }
+    };
+
+    public void onDestroy(){
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+        isLive = false;
+        if (isConnect){
+            isConnect = false;
+            try {
+                if (socket != null){
+                    socket.close();
+                    socket = null;
+                    printWriter.close();
+                    printWriter = null;
+                }
+            }
+
+            catch (IOException e){
+                e.printStackTrace();
+            }
+            thread.interrupt();
+        }
+    }
+
+    @Override
+    protected void onStop(){
+        // TODO Auto-generated method stub
+        super.onStop();
+        isActivity = false;
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        // TODO Auto-generated method stub
+        super.onRestart();
+        isActivity = true;
+        Log.i(TAG, "onRestart");
+    }
+
+    private void loadActivity(Context context, Class<?> c) {
+        Intent intent = new Intent();
+        intent.setClass(context, c);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/android/botcontrol/activity/MainActivity.java
+++ app/src/main/java/com/example/android/botcontrol/activity/MainActivity.java
@@ -1,26 +1,31 @@
 package com.example.android.botcontrol.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.android.botcontrol.R;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private final int SPLASH_DISPLAY_LENGTH = 3000;
+    private final int SPLASH_DISPLAY_LENGTH = 3000; // Duration for Splash Screen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.logo);
+        setContentView(R.layout.logo); // Calls the log.xml file from which it pulls the image for splash screen
 
-        new Handler().postDelayed(new Runnable(){
+         new Handler().postDelayed(new Runnable(){ // Handler is used. Learn Why
 
-           @Override
+          @Override
            public void run(){
                Intent mainIntent = new Intent(MainActivity.this,Control.class);
                MainActivity.this.startActivity(mainIntent);
@@ -39,9 +44,48 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        setContentView(R.layout.main);
         return super.onOptionsItemSelected(item);
+    }
+
+    private void loadActivity(Context context,Class<?> c){
+        Intent intent=new Intent();
+        intent.setClass(context,c);
+        startActivity(intent);
+        overridePendingTransition(R.anim.bottom_in,R.anim.bottom_out);
+    }
+
+    // Options we have after clicking on settings from the Options Menu (3 dots)
+    private void setMainBtnAffairs() {
+        final Button btnConn = (Button) this.findViewById(R.id.btnConn);
+        final Button btnSet = (Button) this.findViewById(R.id.btnSet);
+        final ImageButton imageButtonTest = (ImageButton) this.findViewById(R.id.imageButtonTest);
+
+        //OnClickListner for Connect Button
+        btnConn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadActivity(MainActivity.this,Control.class);
+                Log.d(TAG, "Connection Button");// Asli code mein Chinese
+            }
+        });
+
+        //OnClickListner for Settings Button
+        btnSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadActivity(MainActivity.this,Set.class);
+                Log.d(TAG, "Set Button");
+            }
+        });
+
+        //OnClickListner for Test Button
+        imageButtonTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadActivity(MainActivity.this,TestAct.class);
+                Log.d(TAG, "Test Button");
+            }
+        });
     }
 }
